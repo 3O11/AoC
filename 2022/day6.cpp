54597acf28678c302527bc3d153cb8864f4e3ff7
Viktor Soukup <vitosoukup@gmail.com>
2022-12-06 11:48:51 +0100

    AoC 2022 Day6 (add shortest solution yet)

--- 2022/day6.cpp
+++ 2022/day6.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <cstdint>
+#include <set>
 
 int64_t findUniqueSubstr(const std::string& str, int64_t length)
 {
@@ -17,6 +18,15 @@ int64_t findUniqueSubstr(const std::string& str, int64_t length)
         if (unique) {
             return i + 1;   
         }
+    }
+    return -1;
+}
+
+int64_t findUniqueSubstr2(const std::string& str, int64_t length)
+{
+    for (int64_t i = length; i < str.length(); ++i) {
+        if (std::set(str.begin() + i - length, str.begin() + i).size() == str.substr(i - length + 1, length).size())
+            return i;
     }
     return -1;
 }
