a929b879dcbecd1ed0801c6623418c12385311c7
Viktor Soukup <vitosoukup@gmail.com>
2024-12-02 04:02:48 +0100

    Add puzzle input cache

--- 2024/Common/Source/InputGetter.cpp
+++ 2024/Common/Source/InputGetter.cpp
@@ -1,16 +1,20 @@
 #include "InputGetter.hpp"
 #include "Core.hpp"
 
-#include <__expected/unexpect.h>
-#include <__expected/unexpected.h>
 #include <cpr/cpr.h>
 
+#include <filesystem>
 #include <format>
 #include <fstream>
 #include <iterator>
+#include <optional>
+#include <sstream>
+#include <system_error>
 
 namespace AOC2024
 {
+    namespace fs = std::filesystem;
+
     namespace
     {
         std::expected<std::string, GetInputFailure> LoadSessionCookie()
@@ -26,12 +30,49 @@ namespace AOC2024
                 std::istreambuf_iterator<char>{}
             );
         }
+
+        bool CachePuzzleInput(uint32_t year, uint32_t day, const std::string& input)
+        {
+            std::error_code ec;
+
+            if (!fs::exists("../inputs", ec)) {
+                fs::create_directories("../inputs", ec);
+                if (ec) {
+                    return false;
+                }
+            }
+
+            std::ofstream inputCache(std::format("../inputs/{}D{}.txt", year, day), std::ios::binary | std::ios::trunc);
+            if (!inputCache) {
+                return false;
+            }
+
+            inputCache << input;
+            inputCache.close();
+            return true;
+        }
+
+        std::optional<std::string> LoadPuzzleInput(uint32_t year, uint32_t day)
+        {
+            std::ifstream inputCache(std::format("../inputs/{}D{}.txt", year, day), std::ios::binary);
+            if (!inputCache) {
+                return std::nullopt;
+            }
+
+            std::stringstream inputBuffer;
+            inputBuffer << inputCache.rdbuf();
+            return inputBuffer.str();
+        }
     }
 
     std::expected<std::string, GetInputFailure> GetPuzzleInput(uint32_t year, uint32_t day)
     {
-        auto sessionCookie = LoadSessionCookie();
+        auto cachedInput = LoadPuzzleInput(year, day);
+        if (cachedInput) {
+            return *cachedInput;
+        }
 
+        auto sessionCookie = LoadSessionCookie();
         if (!sessionCookie) {
             return sessionCookie;
         }
@@ -40,6 +81,10 @@ namespace AOC2024
             cpr::Url(std::format("https://adventofcode.com/{}/day/{}/input", year, day)),
             cpr::Cookie("session", sessionCookie.value(), ".adventofcode.com")
         );
+
+        if (!cachedInput) {
+            CachePuzzleInput(year, day, sponse.text);
+        }
 
         if (sponse.status_code == 200) {
             return sponse.text;
