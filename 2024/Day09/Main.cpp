1255cc8b6c35b0801023c4b7e0fc9d028af8b9c3
Viktor Soukup <61194765+3O11@users.noreply.github.com>
2024-12-08 08:42:20 +0100

    AoC 2024 Day 8,9 stubs

---
+++ 2024/Day09/Main.cpp
@@ -0,0 +1,21 @@
+#include "InputGetter.hpp"
+
+#include <print>
+#include <ranges>
+
+int main()
+{
+    namespace ranges = std::ranges;
+    namespace views = std::views;
+
+    auto input = AOC2024::GetPuzzleInput(2024, 7);
+    if (!input) { return 1; }
+
+    std::int32_t result1 = 0;
+    std::int32_t result2 = 0;
+
+    std::println("First solution: {}", result1);
+    std::println("Second solution: {}", result2);
+
+    return 0;
+}
